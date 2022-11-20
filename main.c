95846c21b075765dd79759e2ac191dfc4242c0e8
Maksym Torhunakov <torgunakovmaks2@gmail.com>
2022-11-20 23:41:04 +0100

    Debug working

--- main.c
+++ main.c
@@ -5,65 +5,20 @@
 #include "core_pico.h"
 #include "colorRGB111.h"
 
-// 2 pixels in one uint32 word (32 * 64 : 2 = 1024 words)
-static uint32_t temporary_a[1024];
-static uint8_t temp_rgb111_a[1024];
+#define LED_PICO    25
+#define LED_PICO_W  0
+#define LED LED_PICO
 
 int main() {
     // For diod
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
-    // For oscyloscop
-    gpio_init(22);
-    gpio_set_dir(22, GPIO_OUT);
-
-    // Shared hadlers
-    PICO_IRQ_Set_Handlers();
-
-    // Fill the temporarry buffer for rgb888 colors
-    uint32_t red = 0, green = 0, blue = 0;
-    for (size_t i = 0; i < 2048; i++){
-        /* Normal
-        if (red == 32){
-            red = 0;
-            green += 2;
-        }
-        if (green == 32){
-            green = 0;
-            blue += 2;
-        }
-
-        uint32_t color = red | (green << 5) | (blue << 11);
-        if (i < 1024){
-            temporary_a[i] = color;
-        }
-        else{
-            temporary_a[i - 1024] = color << 16;
-        }
-        red += 4;
-        */
-        /* Test */
-        if (i < 1024){
-            temporary_a[i] = 1 | (1 << 5);
-        }
-        else{
-            temporary_a[i - 1024] = 1 | (1 << 5);
-        }
-    }
-    //hub75_rgb555_set_buffer(temporary_a, 0);
-    //hub75_rgb555_init(pio0, 0, 1, 0, 6, 10, 14, 1000000.0);
-
-    for (size_t i = 0; i < 1024; i++){
-        temp_rgb111_a[i] = RGB111_BLUE | (RGB111_YELLOW << 3);
-    }
-    hub75_rgb111_set_buffer(temp_rgb111_a, 0);
-    hub75_rgb111_init(pio0, 0, 1, 0, 6, 10, 14, 1000000.0);
+    gpio_init(LED);
+    gpio_set_dir(LED, GPIO_OUT);
 
     for(;;){
         // To be sure chip works in normal mode
-        gpio_put(25, true);
+        gpio_put(LED, true);
         sleep_ms(500);
-        gpio_put(25, false);
+        gpio_put(LED, false);
         sleep_ms(500);
     }
 }
