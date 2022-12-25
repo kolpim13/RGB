f6f76a321dbeed0ad09890397edef4e7745c5aee
Maksym Torhunakov <torgunakovmaks2@gmail.com>
2022-12-25 13:30:07 +0100

    RIO main module add

---
+++ RIO/RIO_BASE.c
@@ -0,0 +1,11 @@
+#include "RIO_BASE.h" 
+
+static uint8_t registers[255] __attribute__ ((aligned (4))) = { 1, };
+
+int8_t RIO_REG_DISPLAYMODE_Get(void){
+    return *((int8_t *)&registers[0]);
+}
+void RIO_REG_DISPLAYMODE_Set(int8_t value){
+    *((int8_t *)&registers[0]) = value;
+}
+
---
+++ RIO/RIO_BASE.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "stdint.h"
+
+#define RIO_REG_DISPLAYMODE 0
+
+
+#define RIO_REG_DISPLAYMODE_OFFSET 0
+
+
+#define RIO_REG_DISPLAYMODE_MIN 0
+
+
+#define RIO_REG_DISPLAYMODE_MAX 1
+
+
+int8_t RIO_REG_DISPLAYMODE_Get(void);
+void RIO_REG_DISPLAYMODE_Set(int8_t value);
+
